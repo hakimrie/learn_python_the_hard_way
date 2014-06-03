347bc0978e0ae9193970809799c53a51dad554cf
hakim <hakimrie@gmail.com>
2014-06-03 13:42:43 +0700

    exercise 11 and 13

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called: ", script
+print "Your first variable is :", first
+print "Your second variable is:", second
+print "Your third variable is: ", third
+
+wulan = int(raw_input("Nang bandung pirang wulan? "))
+print "awakmu nang bandung %r wulan" % wulan
